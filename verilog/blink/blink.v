65084662fbb70b32822055c0781064cca8d1bf1d
Greg Davill <greg.davill@gmail.com>
2020-10-08 09:33:32 +1030

    verilog: Update examples

--- verilog/blink/blink.v
+++ verilog/blink/blink.v
@@ -1,4 +1,5 @@
 /* Copyright 2020 Gregory Davill <greg.davill@gmail.com> */
+`default_nettype none
 
 /*
  *  Blink a LED on the OrangeCrab using verilog
@@ -6,20 +7,22 @@
 
 module top (
     input CLK,
-
-	output LED1,
-	output LED2,
-	output LED3
+    output LED1,
+    output LED2,
+    output LED3
 );
+    // Create a 27 bit register
     reg [26:0] counter = 0;
 
-	always @(posedge CLK) begin
-		counter <= counter + 1;
-	end
+    // Every positive edge increment register by 1
+    always @(posedge CLK) begin
+        counter <= counter + 1;
+    end
 
-	assign LED1 = ~counter[24];
+    // Output inverted values of counter onto LEDs
+    assign LED1 = ~counter[24];
     assign LED2 = ~counter[25];
     assign LED3 = 1;
-    
+
 
 endmodule
--- verilog/blink_reset/blink.v
+++ verilog/blink_reset/blink.v
@@ -1,4 +1,5 @@
 /* Copyright 2020 Gregory Davill <greg.davill@gmail.com> */
+`default_nettype none
 
 /*
  *  Blink a LED on the OrangeCrab using verilog
@@ -7,29 +8,33 @@
 module top (
     input CLK,
 
-	output LED1,
-	output LED2,
-	output LED3,
+    output LED1,
+    output LED2,
+    output LED3,
 
-	output RST_N,
-	input BTN_N
+    output RST_N,
+    input BTN_N
 );
+    // Create a 27 bit register
     reg [26:0] counter = 0;
 
-	always @(posedge CLK) begin
-		counter <= counter + 1;
-	end
+    // Every positive edge increment register by 1
+    always @(posedge CLK) begin
+        counter <= counter + 1;
+    end
 
-	assign LED1 = ~counter[24];
+    // Output inverted values of counter onto LEDs
+    assign LED1 = ~counter[24];
     assign LED2 = ~counter[25];
     assign LED3 = 1;
 
-	/* Reset logic on button press */
-	reg reset_sr = 1'b1;
-	always @(posedge CLK) begin
-		reset_sr <= {BTN_N};
-	end
-	assign RST_N = reset_sr;
-    
+    // Reset logic on button press.
+    // this will enter the bootloader
+    reg reset_sr = 1'b1;
+    always @(posedge CLK) begin
+        reset_sr <= {BTN_N};
+    end
+    assign RST_N = reset_sr;
+
 
 endmodule
