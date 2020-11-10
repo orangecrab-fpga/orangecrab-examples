f83a7256f73c88d4ce3c53bb3d601afda7d3ee72
Greg Davill <greg.davill@gmail.com>
2020-11-10 20:18:05 +1030

    verilog: Update r0.2 lpf

--- verilog/blink/blink.v
+++ verilog/blink/blink.v
@@ -6,23 +6,23 @@
  */
 
 module top (
-    input CLK,
-    output LED1,
-    output LED2,
-    output LED3
+    input clk48,
+    output rgb_led0_r,
+    output rgb_led0_g,
+    output rgb_led0_b
 );
     // Create a 27 bit register
     reg [26:0] counter = 0;
 
     // Every positive edge increment register by 1
-    always @(posedge CLK) begin
+    always @(posedge clk48) begin
         counter <= counter + 1;
     end
 
     // Output inverted values of counter onto LEDs
-    assign LED1 = ~counter[24];
-    assign LED2 = ~counter[25];
-    assign LED3 = 1;
+    assign rgb_led0_r = ~counter[24];
+    assign rgb_led0_g = ~counter[25];
+    assign rgb_led0_b = 1;
 
 
 endmodule
--- verilog/blink_reset/blink.v
+++ verilog/blink_reset/blink.v
@@ -6,35 +6,35 @@
  */
 
 module top (
-    input CLK,
+    input clk48,
 
-    output LED1,
-    output LED2,
-    output LED3,
+    output rgb_led0_r,
+    output rgb_led0_g,
+    output rgb_led0_b,
 
-    output RST_N,
-    input BTN_N
+    output rsn_n,
+    input usr_btn
 );
     // Create a 27 bit register
     reg [26:0] counter = 0;
 
     // Every positive edge increment register by 1
-    always @(posedge CLK) begin
+    always @(posedge clk48) begin
         counter <= counter + 1;
     end
 
     // Output inverted values of counter onto LEDs
-    assign LED1 = ~counter[24];
-    assign LED2 = ~counter[25];
-    assign LED3 = 1;
+    assign rgb_led0_r = ~counter[24];
+    assign rgb_led0_g = ~counter[25];
+    assign rgb_led0_b = 1;
 
     // Reset logic on button press.
     // this will enter the bootloader
     reg reset_sr = 1'b1;
-    always @(posedge CLK) begin
-        reset_sr <= {BTN_N};
+    always @(posedge clk48) begin
+        reset_sr <= {usr_btn};
     end
-    assign RST_N = reset_sr;
+    assign rsn_n = reset_sr;
 
 
 endmodule
